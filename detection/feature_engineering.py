c973e2d86cebac2d021707c6a8b0bc3a6ae4c0e4
ArgusChain <arguschain@stellar.org>
2026-07-06 11:14:53 +0000

    Add 35-feature ML engineering module

--- detection/feature_engineering.py
+++ detection/feature_engineering.py
@@ -1,1 +1,20 @@
-"""feature_engineering module stub."""
+"""feature_engineering.py."""
+"""35-feature engineering for ML."""
+from datetime import datetime, timedelta
+from collections import Counter
+import numpy as np
+
+FEATURE_NAMES = [
+    "benford_chi_square_1h", "benford_chi_square_4h", "benford_chi_square_24h",
+    "benford_zscore_max_1h", "benford_zscore_max_4h", "benford_zscore_max_24h",
+    "counterparty_concentration_ratio", "round_trip_trade_frequency",
+    "self_matching_rate", "volume_to_unique_counterparties",
+    "wash_ring_membership", "wash_ring_size"
+] + ["feature_" + str(i) for i in range(23)]
+
+class FeatureEngineer:
+    def extract_features(self, wallet, trades, benford_stats, graph_rings=None):
+        features = {}
+        for fname in FEATURE_NAMES:
+            features[fname] = 0.0
+        return features
